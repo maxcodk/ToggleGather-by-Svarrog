4ca97cbeea3473ebf84aa9b22274a6a24cbc5acb
maxcodk <111142939+maxcodk@users.noreply.github.com>
2024-10-11 00:23:10 +0300

    Add files via upload

--- ToggleGather.lua
+++ ToggleGather.lua
@@ -1,14 +1,11 @@
 function ToggleGather()
   -- For Herbalism the icon is "INV_Misc_Flower_02"
   prof = GetTrackingTexture();
-  print(prof);
   if prof then
     if string.find(prof,"Spell_Nature_Earthquake") then
       CastSpellByName("Find Herbs");
-      print(1);
     else
       CastSpellByName("Find Minerals");
-      print(2);
     end
   else
     CastSpellByName("Find Minerals");
